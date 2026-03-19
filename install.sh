45feb92a6040f0e2c3ad6189f1cefc460c2d5020
someguythat-thinkswithportals <rozavkoa@gmail.com>
2026-03-19 12:24:21 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #setup script for putting the networkmanager file in arch linux
 
-cd bin
-sudo mkdir /usr/local/bin/netmgrfix
-sudo networkmanagerfixer.sh /usr/local/bin/netmgrfix
-sudo chmod +x /usr/local/bin/netmgrfix
-sudo chown root:root /usr/local/bin netmgrfix
+cd bin || { echo "Directory 'bin' not found"; exit 1; }
+sudo mkdir -p /usr/local/bin/netmgrfix
+sudo cp networkmanagerfixer.sh /usr/local/bin/netmgrfix/
+sudo chmod +x /usr/local/bin/netmgrfix/networkmanagerfixer.sh
+sudo chown root:root /usr/local/bin/netmgrfix/networkmanagerfixer.sh
