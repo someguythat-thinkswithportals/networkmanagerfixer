0bc6d5a8609640b0ff70e453441451a9539e51fb
someguythat-thinkswithportals <rozavkoa@gmail.com>
2026-03-22 21:00:24 +0300

    deleted install.sh
    deleted the broken install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#setup script for putting the networkmanager file in arch linux
-# WARNING, THE SCRIPT IS BROKEN
-cd bin || { echo "Directory 'bin' not found"; exit 1; }
-sudo mkdir -p /usr/local/bin/netmgrfix
-sudo cp networkmanagerfixer.sh /usr/local/bin/netmgrfix/
-sudo chmod +x /usr/local/bin/netmgrfix/networkmanagerfixer.sh
-sudo chown root:root /usr/local/bin/netmgrfix/networkmanagerfixer.sh
